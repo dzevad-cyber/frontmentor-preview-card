bae057c5ef4482588d2c06d93440d0323307c684
Dzevad <dzevad.del@gmail.com>
2022-03-09 01:22:19 +0100

    finished mobile design and mobile design with active states

--- shareBtnAnimationMobile.js
+++ shareBtnAnimationMobile.js
@@ -1,6 +1,14 @@
 "use strict";
 const btnShare = document.querySelector('.preview-card__btn-share');
 const previewCardCreatorBox = document.querySelector('.preview-card__creator-box');
+const previewCardShare = document.querySelector('.preview-card__share');
+const previewCardCreatorWrapper = document.querySelector('.preview-card__creator-wrapper');
+const iconBtn = document.querySelector('.icon-btn');
+const previewCardBtnShare = document.querySelector('.preview-card__btn-share');
 btnShare.addEventListener('click', (e) => {
     previewCardCreatorBox.classList.toggle('preview-card__creator-box--onClick');
+    previewCardShare.classList.toggle('preview-card__share--onClick');
+    previewCardCreatorWrapper.classList.toggle('preview-card__creator-wrapper--onClick');
+    iconBtn.classList.toggle('icon-btn--onClick');
+    previewCardBtnShare.classList.toggle('preview-card__btn-share--onClick');
 });
--- shareBtnAnimationMobile.ts
+++ shareBtnAnimationMobile.ts
@@ -6,6 +6,25 @@ const previewCardCreatorBox = document.querySelector(
   '.preview-card__creator-box'
 )! as HTMLDivElement;
 
+const previewCardShare = document.querySelector(
+  '.preview-card__share'
+)! as HTMLUListElement;
+
+const previewCardCreatorWrapper = document.querySelector(
+  '.preview-card__creator-wrapper'
+)! as HTMLElement;
+
+const iconBtn = document.querySelector('.icon-btn')! as HTMLElement;
+const previewCardBtnShare = document.querySelector(
+  '.preview-card__btn-share'
+)! as HTMLButtonElement;
+
 btnShare.addEventListener('click', (e: Event): void => {
   previewCardCreatorBox.classList.toggle('preview-card__creator-box--onClick');
+  previewCardShare.classList.toggle('preview-card__share--onClick');
+  previewCardCreatorWrapper.classList.toggle(
+    'preview-card__creator-wrapper--onClick'
+  );
+  iconBtn.classList.toggle('icon-btn--onClick');
+  previewCardBtnShare.classList.toggle('preview-card__btn-share--onClick');
 });
